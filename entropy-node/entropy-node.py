c2652fa8b49e319ec1cea1befce5b73410ab1392
Pratish Chaudhary <pratishchaudhary123@gmail.com>
2026-03-22 10:21:53 +0545

    Add solution for entropy-node

---
+++ entropy-node/entropy-node.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def entropy_node(y):
+    """
+    Compute entropy for a single node using stable logarithms.
+    """
+    # Write code here
+    total = len(y)
+    unique_vals, counts = np.unique(y, return_counts=True)
+    proba = counts / total
+    proba = np.array(proba)
+    if (proba == 1).any():
+      return -0.0
+
+    E = -np.sum(proba * np.log2(proba))
+    return E
+
