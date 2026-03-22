554570a4f0618fac506a136de589bdbb623124d7
Pratish Chaudhary <pratishchaudhary123@gmail.com>
2026-03-22 10:22:09 +0545

    Update solution for entropy-node

--- entropy-node/entropy-node.py
+++ entropy-node/entropy-node.py
@@ -8,7 +8,6 @@ def entropy_node(y):
     total = len(y)
     unique_vals, counts = np.unique(y, return_counts=True)
     proba = counts / total
-    proba = np.array(proba)
     if (proba == 1).any():
       return -0.0
 
